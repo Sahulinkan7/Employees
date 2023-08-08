eb2abeb5db2f8aee4b4720bd8e75ece49b33cff3
Linkan Sahu <sahulinkan7@gmail.com>
2023-08-08 17:31:11 +0530

    migrated employee table

--- empprofile/models.py
+++ empprofile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=40)
+    email=models.EmailField()
+    phone=models.CharField(max_length=12,blank=True)
+    image=models.ImageField(upload_to="images")
+    
+    def __str__(self) -> str:
+        return self.first_name
