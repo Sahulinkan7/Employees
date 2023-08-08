92ba02d11844d832f59cf91cc0b5f09bbb222316
Linkan Sahu <sahulinkan7@gmail.com>
2023-08-08 18:03:03 +0530

    code updated

--- empprofile/views.py
+++ empprofile/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Employee
 # Create your views here.
+
+def home(request):
+    employees=Employee.objects.all()
+    return render(request,"empprofile/index.html",{'employees':employees})
+
+def empdetail(request,pk):
+    emp=Employee.objects.get(id=pk)
+    return render(request,"empprofile/empdetails.html",{'emp':emp})
